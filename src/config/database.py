9ad7357aca034882efd67d348ea57bc217ddf311
MiguelAP30 <miguelonperez300@gmail.com>
2024-05-04 17:30:04 -0500

    se organizó el gitIgnore y se solucionó el problema del uvicorn

--- src/config/database.py
+++ src/config/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 
-database_url = "sqlite:///./database.sqlite"
+database_url = "sqlite:///./GymHouseDB.sqlite"
 
 engine = create_engine(database_url,native_datetime=True,connect_args={ "check_same_thread": False})
 
