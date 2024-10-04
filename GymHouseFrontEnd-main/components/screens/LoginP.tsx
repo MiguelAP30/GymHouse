8bb095edaf0cabb690f07b5afa40db2c6fd2b157
andresperez2003 <andresap2017@gmail.com>
2024-10-04 15:55:33 -0500

    Fixing messages of inputs in login component

--- GymHouseFrontEnd-main/components/screens/LoginP.tsx
+++ GymHouseFrontEnd-main/components/screens/LoginP.tsx
@@ -3,106 +3,105 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { useForm, SubmitHandler } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import {boldFormText, btnIngresar, centerItem, hoverscale, inputForm} from '../tokens';
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { boldFormText, btnIngresar, centerItem, hoverscale, inputForm } from '../tokens';
 import { metadataLogin } from '@/components/metadata';
 export { metadataLogin } from '@/components/metadata';
 import { Login } from '@/types/login';
 import { postLogin } from '@/libs/api_general';
 import { useRouter } from 'next/navigation';
 import Swal from 'sweetalert2';
-import { loginSchema } from "@/validators/loginSchema"
+import { loginSchema } from "@/validators/loginSchema";
 
 export default function LoginP() {
     const router = useRouter();
     const {
-            register,
-            handleSubmit,
-            watch,
-            formState: { errors },
-        } = useForm<Login>({
-            resolver: zodResolver(loginSchema)
-        })
-        const onSubmit: SubmitHandler<Login> = async (data) => {
-        console.log(data)
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Login>({
+        resolver: zodResolver(loginSchema),
+    });
+
+    const onSubmit: SubmitHandler<Login> = async (data) => {
+        console.log(data);
         const body = {
             email: data.email,
-            password: data.password
-        }
-        let token = null
-        token = await postLogin(body)
+            password: data.password,
+        };
+        let token = null;
+        token = await postLogin(body);
         console.log(await postLogin(body));
-        if(token){
+        if (token) {
             localStorage.setItem('token', token);
-            router.push('/')
+            router.push('/');
             Swal.fire({
                 title: "!Bienvenido!",
                 text: "Usuario logueado correctamente",
-                icon: "success"
+                icon: "success",
             });
-        } else{
+        } else {
             Swal.fire({
                 title: "!Error!",
                 text: "El usuario no ha podido loguearse",
-                icon: "error"
+                icon: "error",
             });
         }
-    }
-
-
+    };
 
     return (
         <>
-        <meta name="description" content={metadataLogin.description ?? ''} />
-        <title>{metadataLogin.title?.toString() ?? ''}</title>
+            <meta name="description" content={metadataLogin.description ?? ''} />
+            <title>{metadataLogin.title?.toString() ?? ''}</title>
 
-        
-        <div className={`min-h-screen  ${centerItem} flex-col`} >
-            <div className={`mb-5 ${centerItem} flex-col`}>
-                <Link href="/">
-                    <Image 
-                        className={hoverscale}
-                        src='/logo_gym.png'
-                        alt="logo del gimnasio" 
-                        width={350}
-                        height={300}
-                    />
-                </Link>
-                <h1 className={`${boldFormText}`}>
-                    Welcome to GymHouse
-                </h1>
+            <div className={`min-h-screen  ${centerItem} flex-col`}>
+                <div className={`mb-5 ${centerItem} flex-col`}>
+                    <Link href="/">
+                        <Image
+                            className={hoverscale}
+                            src='/logo_gym.png'
+                            alt="logo del gimnasio"
+                            width={350}
+                            height={300}
+                        />
+                    </Link>
+                    <h1 className={`${boldFormText}`}>
+                        Welcome to GymHouse
+                    </h1>
+                </div>
+                <form className={`${inputForm} p-4 rounded-2xl border border-gray-500`} onSubmit={handleSubmit(onSubmit)}>
+                    <div className="flex flex-col mb-4">
+                        <label className="text-white" htmlFor="email">Email</label>
+                        <input
+                            {...register("email")}
+                            id="email"
+                            type="text"
+                            className="p-2 rounded text-black"
+                        />
+                        {/* Mostrar el mensaje de error para el email */}
+                        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+                    </div>
+                    <div className={`${inputForm}`}>
+                        <label className="text-white" htmlFor="password">Password</label>
+                        <input
+                            {...register("password")}
+                            id="password"
+                            type="password"
+                            className="p-2 rounded text-black"
+                        />
+                        {/* Mostrar el mensaje de error para el password */}
+                        {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+                    </div>
+                    <div className="mt-5">
+                        <input
+                            type="submit"
+                            value="Ingresar"
+                            className={`${btnIngresar} w-full`}
+                        />
+                    </div>
+                </form>
             </div>
-            <form className={`${inputForm} p-4 rounded-2xl border border-gray-500`} onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex flex-col mb-4">
-                    <label className="text-white" htmlFor="user">Email</label>
-                    <input 
-                        {...register("email", { required: true })}
-                        id="email" 
-                        type="text" 
-                        className="p-2 rounded text-black" 
-                    />
-                    {errors && <p className="text-red-500">{}</p>}
-                </div>
-                <div className={`${inputForm}`}>
-                    <label className="text-white" htmlFor="password">Password</label>
-                    <input 
-                        {...register("password", { required: true })}
-                        id="password" 
-                        type="password" 
-                        className="p-2 rounded text-black" 
-                    />
-                    {errors && <p className="text-red-500">{}</p>}
-                </div>
-                <div className="mt-5">
-                    <input
-                        type="submit"
-                        value="Ingresar"
-                        className={`${btnIngresar} w-full`}
-                    />
-                </div>
-            </form>
-        </div>
         </>
     );
 }
--- GymHouseFrontEnd-main/validators/loginSchema.ts
+++ GymHouseFrontEnd-main/validators/loginSchema.ts
@@ -1,11 +1,12 @@
-import { z } from 'zod'
+import { z } from 'zod';
 
 export const loginSchema = z.object({
-    email: z.string()
-        .email({ message: "Correo inválido" })
-        .min(5, { message: "El campo debe tener minimo 5 caracteres." })
-        .max(50, { message: "El campo no puede superar los 50 caracteres." }),
+    email: z.string()        
+        .min(1, { message: "El campo no puede estar vacio." })
+        .max(50, { message: "El campo no puede superar los 50 caracteres." })
+        .email({ message: "Correo inválido" }),
         
     password: z.string()
-                .max(30, { message: "El campo no puede superar los 30 caracteres" })
-})
+        .max(30, { message: "El campo no puede superar los 30 caracteres" })
+        .min(1, { message: "El campo no puede estar vacío." })
+});
